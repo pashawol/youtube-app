55e6c17bb533a416420ca8058183a6e7f43c9af0
PAVEL KHALUNIN <wol1414@gmail.com>
2024-07-08 21:55:39 +0200

    fix: components structure

---
+++ youTube-client/src/app/components/filter-block/buttons-for-sort/buttons-for-sort.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "app-buttons-for-sort",
+    standalone: true,
+    imports: [],
+    templateUrl: "./buttons-for-sort.component.html",
+    styleUrl: "./buttons-for-sort.component.scss"
+})
+export class ButtonsForSortComponent {}
