2e12c7e53ad948123b5b300c95836e0ae6ead4da
PAVEL KHALUNIN <wol1414@gmail.com>
2024-07-18 22:58:19 +0200

    feat: done detail page

--- youTube-client/src/app/pages/youtube/video-details/containers/video-details/video-details.component.ts
+++ youTube-client/src/app/pages/youtube/video-details/containers/video-details/video-details.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from "@angular/common"
 import { Component } from "@angular/core"
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser"
 import { ActivatedRoute } from "@angular/router"
 import { Item } from "@shared/models/response.model"
 import { Observable } from "rxjs"
 
+import { HighlightDirective } from "../../../directives/highlight.directive"
 import { RequestService } from "../../services/request.service"
 
 @Component({
     selector: "app-video-details",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, HighlightDirective],
     templateUrl: "./video-details.component.html",
     styleUrl: "./video-details.component.scss"
 })
@@ -18,10 +20,15 @@ export class VideoDetailsComponent {
 
     constructor(
         private requestService: RequestService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private sanitizer: DomSanitizer
     ) {}
 
     private getVideoDetails(id: string | null): Observable<Item> {
         return this.requestService.fetchData(id)
+    }
+    getSafeUrl(videoId: string): SafeResourceUrl {
+        const url = `https://www.youtube.com/embed/${videoId}`
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url)
     }
 }
