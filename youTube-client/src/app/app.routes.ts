76fdcde6f5eaa5eafe125fffcc159f1063b2a180
PAVEL KHALUNIN <wol1414@gmail.com>
2024-07-19 00:04:50 +0200

    refactor: edit router

--- youTube-client/src/app/app.routes.ts
+++ youTube-client/src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from "@angular/router"
 import { ErrorModule } from "@pages/error/error.module"
 import { LoginModule } from "@pages/login/login.module"
-import { YoutubeModule } from "@pages/youtube/youtube.module"
+import { error } from "console"
+// import { YoutubeModule } from "@pages/youtube/youtube.module"
 
 export const routes: Routes = [
-    { path: "", loadChildren: () => YoutubeModule },
+    { path: "", redirectTo: "login", pathMatch: "full" },
     {
         path: "login",
         loadChildren: () => LoginModule
     },
     {
-        path: "**",
+        path: "error",
         loadChildren: () => ErrorModule
+    },
+
+    {
+        path: "**",
+        redirectTo: "error"
     }
 ]
