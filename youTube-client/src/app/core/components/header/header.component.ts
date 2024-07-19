bdf8595b79f247c7c2b4ee8d74d2382a10b847cb
PAVEL KHALUNIN <wol1414@gmail.com>
2024-07-20 00:14:42 +0200

    feat: add logout button

--- youTube-client/src/app/core/components/header/header.component.ts
+++ youTube-client/src/app/core/components/header/header.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from "@angular/common"
 import { Component } from "@angular/core"
+import { LoginService } from "@app/pages/login/services/login.service"
+import { ButtonComponent } from "@app/shared/components"
 
 import { LoginInformationBlockComponent, LogoComponent, SearchBlockComponent, SettingsButtonComponent } from ".."
 
 @Component({
     selector: "app-header",
     standalone: true,
-    imports: [SearchBlockComponent, LogoComponent, LoginInformationBlockComponent, SettingsButtonComponent],
+    imports: [
+        SearchBlockComponent,
+        LogoComponent,
+        LoginInformationBlockComponent,
+        SettingsButtonComponent,
+        ButtonComponent,
+        CommonModule
+    ],
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.scss"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    constructor(private loginService: LoginService) {}
+
+    logout() {
+        this.loginService.logout()
+    }
+
+    isLoggedIn(): boolean {
+        return this.loginService.isLoggedIn()
+    }
+}
