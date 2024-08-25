8b90716edae1f94eba3608b49357e2f478ead8ea
PAVEL KHALUNIN <wol1414@gmail.com>
2024-08-25 15:55:57 +0200

    refactro: component`s test

--- youTube-client/src/app/core/components/filter-block/filter-block.component.spec.ts
+++ youTube-client/src/app/core/components/filter-block/filter-block.component.spec.ts
@@ -1,4 +1,9 @@
+import { CommonModule } from "@angular/common"
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { FormsModule } from "@angular/forms"
+import { FilterService } from "@app/pages/youtube/pages/search/services/filter.service"
+import { ButtonComponent } from "@shared/components"
+import { InputTextModule } from "primeng/inputtext"
 
 import { FilterBlockComponent } from "./filter-block.component"
 
@@ -8,15 +13,26 @@ describe("FilterBlockComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FilterBlockComponent]
+            imports: [FilterBlockComponent, CommonModule, ButtonComponent, FormsModule, InputTextModule],
+            providers: [
+                {
+                    provide: FilterService,
+                    useValue: {
+                        setFilterCriteria: jest.fn()
+                    }
+                }
+            ]
         }).compileComponents()
 
         fixture = TestBed.createComponent(FilterBlockComponent)
         component = fixture.componentInstance
-        fixture.detectChanges()
     })
 
     it("should create", () => {
         expect(component).toBeTruthy()
+    })
+
+    it("should initialize with default filter criteria", () => {
+        expect(component.filterCriteria).toEqual({ date: "", count: "", searchText: "" })
     })
 })
